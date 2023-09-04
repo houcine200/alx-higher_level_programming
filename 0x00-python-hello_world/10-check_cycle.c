6f72eb718681e8538ed8501b15b18a3066219190
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-04 05:31:25 -0700

    task 10 add fast next

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *fast = list;
 	
-	while(slow && fast)
+	while(slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
