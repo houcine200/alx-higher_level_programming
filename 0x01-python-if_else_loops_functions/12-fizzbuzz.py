c7080ed5a9e7a256e93ff6a6f0b8f158e9b87ce7
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-05 08:29:23 -0700

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz", end="")
+        elif (n % 3 == 0):
+            print("Fizz", end="")
+        elif (n % 5 == 0):
+            print("Buzz", end="")
+        else:
+            print(n, end="")
+        if (n != 100):
+            print(end=" ")
+        else:
+            print(end="\n")
