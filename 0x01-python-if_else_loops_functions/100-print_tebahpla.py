b572afe119a6b52a6993f417d8fdea53adb53225
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-05 10:24:10 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a') - 1, -1):
-    if(c % 2 == 0):
-        print(chr(c), end="")
-    else:
-        print(chr(c - 32), end="")
+    if(c % 2 != 0):
+        c -= 32
+    print("{}".format(chr(c)), end="")
