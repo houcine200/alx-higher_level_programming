ed3bfd6ae71b049c45e0919c4d2de58e492c1f35
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-07 04:40:31 -0700

    task 2 1 arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,11 @@ if __name__ == '__main__':
     import sys
 
 leng = len(sys.argv) - 1
+
 if leng == 0:
     print("{} arguments.".format(leng))
+elif leng == 1:
+    print("{} argument:".format(leng))
 else:
     print("{} arguments:".format(leng))
 for i in range(1, len(sys.argv)):
