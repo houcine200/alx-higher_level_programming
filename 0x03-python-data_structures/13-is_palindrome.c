5909685851c29c246da5de686445c828d997077f
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-09 14:58:17 +0000

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,4 @@
-#include"lists.h"
+#include "lists.h"
 
 int is_palindrome(listint_t **head);
 
@@ -18,23 +18,28 @@ listint_t *reverse_list(listint_t **head)
 }
 int is_palindrome(listint_t **head)
 {
+    listint_t *copy;
+    listint_t *curr;
+    listint_t *reversed;
+    listint_t *list1;
+    listint_t *list2;
+    listint_t *current;
+
     if (*head == NULL || (*head)->next == NULL)
         return 1;
-
-    // Create a copy of the original list
-    listint_t *copy = NULL;
-    listint_t *curr = *head;
+        
+    copy = NULL;
+    curr = *head;
+    
     while (curr != NULL)
     {
         add_nodeint_end(&copy, curr->n);
         curr = curr->next;
     }
-    // Reverse the copy
-    listint_t *reversed = reverse_list(&copy);
+    reversed = reverse_list(&copy);
 
-    // Compare the original list with the reversed copy
-    listint_t *list1 = *head;
-    listint_t *list2 = reversed;
+    list1 = *head;
+    list2 = reversed;
 
     while (list1 != NULL && list2 != NULL)
     {
@@ -48,16 +53,12 @@ int is_palindrome(listint_t **head)
                 copy = copy->next;
                 free(current);
             }
-
-
-            return 0; // Lists are not equal
+            return 0;
         }
         list1 = list1->next;
         list2 = list2->next;
     }
-
-    listint_t *current;
-
+    
     while (copy != NULL)
     {
         current = copy;
