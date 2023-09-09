c029173a7fc4fc0f34e380889c8721d4f1a10214
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-09 07:14:01 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,21 +1,71 @@
 #include"lists.h"
 
+int is_palindrome(listint_t **head);
+
+listint_t *reverse_list(listint_t **head)
+{
+    listint_t *prev = NULL;
+    listint_t *next = NULL;
+
+    while (*head)
+    {
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
+    }
+
+    *head = prev;
+
+    return (*head);
+}
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    static listint_t *temp;
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
 
-    if (current == NULL)
-        return (1);
-
-    if (temp == NULL)
-        temp = current;
-
-    if (is_palindrome(&current->next) && temp->n == current->n)
+    // Create a copy of the original list
+    listint_t *copy = NULL;
+    listint_t *curr = *head;
+    while (curr != NULL)
     {
-        temp = temp->next;
-        return (1);
+        add_nodeint_end(&copy, curr->n);
+        curr = curr->next;
     }
-    else
-        return (0);
+    // Reverse the copy
+    listint_t *reversed = reverse_list(&copy);
+
+    // Compare the original list with the reversed copy
+    listint_t *list1 = *head;
+    listint_t *list2 = reversed;
+
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->n != list2->n)
+        {
+            listint_t *current;
+
+            while (copy != NULL)
+            {
+                current = copy;
+                copy = copy->next;
+                free(current);
+            }
+
+
+            return 0; // Lists are not equal
+        }
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+
+    listint_t *current;
+
+    while (copy != NULL)
+    {
+        current = copy;
+        copy = copy->next;
+        free(current);
+    }
+    return (list1 == NULL && list2 == NULL);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,9 +19,12 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *reverse_list(listint_t **head);
+
 
 #endif /* LISTS_H */
