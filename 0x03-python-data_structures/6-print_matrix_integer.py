c3e84c43beacf477e24ce7a53c1b3fad38ccabc4
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-09 04:06:39 -0700

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for n in row:
-            print("{:d}".format(n), end=" ")
+            if n != row[-1]:
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d}".format(n), end="")
         print()
