fc143cbf00685c13818d07e9a4eb9ca295edf1a7
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-13 08:09:15 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    # return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    if not a_dictionary:
+        return None
+    top = (max(a_dictionary.values()))
+    for k, v in a_dictionary.items():
+        if v == top:
+            return k
