7924954a9c1efb1c239bc634966fbf09d1bbc78d
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-13 13:05:17 -0700

    102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(my_dict, value):
     targets = []
     for key, val in my_dict.items():
-        if key_val is value:
+        if val is value:
             targets.append(key)
     for x in targets:
         del my_dict[x]
