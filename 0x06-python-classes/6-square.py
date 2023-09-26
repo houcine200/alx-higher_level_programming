9391732f2b28234e0d38e2c602a5a838adb4220f
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-26 11:21:58 -0700

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,11 +7,11 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
-        
+
     @property
     def size(self):
         return self.__size
-        
+
     @size.setter
     def size(self, value):
         if isinstance(value, int) is False:
@@ -19,11 +19,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         if isinstance(value, tuple) is False or \
@@ -32,7 +32,7 @@ class Square:
                 all(num >= 0 for num in value) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
