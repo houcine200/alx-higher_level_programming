b85d8fc8d6221275f3d4287f2830e3507ff57544
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-02 18:09:23 +0100

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -47,4 +47,4 @@ class Rectangle:
         return '\n'.join(result)
 
     def __repr__(self):
-        return "Rectangle ({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
