3d4f3e5cdaf3f14015f2fbf3d8adaf00c41b0f65
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-02 13:05:33 +0100

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        return (self.width + self.height) * 2
+        return (self.__width + self.__height) * 2
