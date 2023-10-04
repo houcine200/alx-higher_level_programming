8e844aa7eb7e50d20a47daee97b2f3ce2a41c653
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 13:17:16 +0100

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Module: say_my_name
-prints the first and last name if both are strings.
+Say my name prints
+>>> say_my_name('houcine', 'walaq')
+Outuput, houcine walaq
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    say_my_name prints out the input strings prepended with "My name is".
-    Raises value error if both first_name and last_name are not strings
-    Can accept one or two arguments
-    """
-    if first_name is None or isinstance(first_name, str) is False:
-        raise TypeError("first_name must be a string")
-    if last_name is None or isinstance(last_name, str) is False:
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    if not isinstance(first_name, str) or first_name is None:
+        raise TypeError('first_name must be a string')
+
+    if not isinstance(last_name, str) or last_name is None:
+        raise TypeError('last_name must be a string')
+
+    print("My name is {0:s} {1:s}".format(first_name, last_name))
