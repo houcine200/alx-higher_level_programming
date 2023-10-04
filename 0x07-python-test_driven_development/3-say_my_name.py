d7d6b698bc3ce32f7384ba3004176a9708cb305f
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 13:15:18 +0100

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,6 +13,6 @@ def say_my_name(first_name, last_name=""):
     """
     if first_name is None or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if last_name or not isinstance(last_name, str):
+    if last_name is None or not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
