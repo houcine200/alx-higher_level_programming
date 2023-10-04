e7d7413e95a30874d05a9c4fc952cd5e6c502a62
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 13:13:49 +0100

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,8 +11,8 @@ def say_my_name(first_name, last_name=""):
     Raises value error if both first_name and last_name are not strings
     Can accept one or two arguments
     """
-    if not isinstance(first_name, str):
+    if first_name is None or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if last_name or not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
