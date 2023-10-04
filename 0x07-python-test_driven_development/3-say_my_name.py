dbc58beee31c5b24ca64b610c14d623466eeb41f
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 13:19:42 +0100

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Say my name prints
->>> say_my_name('houcine', 'walaq')
-Outuput, houcine walaq
-"""
+""" Doc """
 
-
-def say_my_name(first_name, last_name=""):
-
-    if not isinstance(first_name, str) or first_name is None:
-        raise TypeError('first_name must be a string')
-
-    if not isinstance(last_name, str) or last_name is None:
-        raise TypeError('last_name must be a string')
-
-    print("My name is {0:s} {1:s}".format(first_name, last_name))
+def say_my_name(first_name="", last_name=""):
+    """ Doc """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
