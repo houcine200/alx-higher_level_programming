58dfd03c669e733a78b0fd15574e5622a3c84d08
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 11:34:06 +0100

    task 2 corr

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -31,6 +31,9 @@ def matrix_divided(matrix, div):
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
