76b29a6325a6b937d9c46a5e244ad5acab7c0005
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-04 13:11:15 +0100

    3j

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,19 +5,14 @@ prints the first and last name if both are strings.
 """
 
 
-def say_my_name(first_name="", last_name=""):
-    """Method for printing first and last name.
-
-    Args:
-        first_name: first name string.
-        last_name: last name string.
-
-    Raises:
-        TypeError: If first_name or last_name are not strings.
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name prints out the input strings prepended with "My name is".
+    Raises value error if both first_name and last_name are not strings
+    Can accept one or two arguments
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
