cab6b1ca65cd693f6351ba4fdd6fdc32e099d111
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-09 16:08:13 +0100

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Implementing a Geometry class"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle classe"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
