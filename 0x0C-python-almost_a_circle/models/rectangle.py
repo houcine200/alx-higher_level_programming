19427b735d3a4c35822b2f476cdead7b1959d05b
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-12 20:57:36 +0100

    task 2 start

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from models.base import Base
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
