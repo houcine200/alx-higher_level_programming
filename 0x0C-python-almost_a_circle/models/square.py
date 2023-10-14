abe9f6848ac24501f1315ef20c3df4b6d72ce382
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-14 11:42:17 +0100

    11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,4 +8,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
     def __str__(self):
-        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.width)
+        return "[{}] ({}) {}/{} - {}".format\
+            (self.__class__.__name__, self.id, self.x, self.y, self.width)
+    
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
