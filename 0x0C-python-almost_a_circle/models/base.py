d0ff12aa51dfe3b6506aa60412efc2ac988ac35e
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-16 16:29:33 +0100

    pycode styyle for base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,23 +3,25 @@
 from json import dumps, loads
 import csv
 
+
 class Base:
     """class representing a base object with an optional unique ID."""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file """
@@ -32,12 +34,13 @@ class Base:
                 for obj in list_objs:
                     lista.append(obj.to_dictionary())
                 file.write(cls.to_json_string(lista))
-    
+
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or len(json_string) == 0:
             return []
         return loads(json_string)
+
     @classmethod
     def create(cls, **dictionary):
         '''Loads instance from dictionary.'''
@@ -62,9 +65,9 @@ class Base:
                 for dic in j_file:
                     instance_list.append(cls.create(**dic))
                 return instance_list
-        except:
+        except FileNotFoundError:
             return []
-        
+
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """ serializes to Csv """
@@ -101,5 +104,3 @@ class Base:
                 inst = cls.create(**instance_dict)
                 instance_array.append(inst)
             return instance_array
-        
-    
