06f0d347593197dcdb3c2df4d6d9a54058cf72ea
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-11 16:22:03 +0100

    9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,4 +4,4 @@ function add(a, b) {
   return a + b;
 }
 
-console.log(add(parseInt(process.argv[3]), parseInt(process.argv[4])));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
