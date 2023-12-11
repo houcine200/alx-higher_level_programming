93d05b0d5837a9904c765bca715fc91a462487d2
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-11 15:35:12 +0100

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const isNum = parseInt(process.argv[2]);
 
 if (isNaN(isNum)) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < isNum; i++) {
     console.log('C is fun');
