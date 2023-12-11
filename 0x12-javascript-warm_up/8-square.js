9e9ad75d9a4d5912765076711baee9d63787cda9
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-11 16:11:08 +0100

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,16 @@
 const size = parseInt(process.argv[2]);
 let row = '';
 
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    row += ('X');
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      row += ('X');
+    }
+    if (i !== size - 1) {
+      row += ('\n');
+    }
   }
-  if (i != size - 1)
-    row += ('\n');
+  console.log(row);
 }
-console.log(row);
