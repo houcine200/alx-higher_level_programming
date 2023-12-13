466b1a6f20889e57d0ef6a070369ad7f916c6ee6
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-13 11:48:48 +0100

    101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-const new_dict = {};
-for (key in dict)
-{
-  if (dict[key] in new_dict) {
-    new_dict[dict[key]].push(key);
+const newDict = {};
+for (const key in dict) {
+  if (dict[key] in newDict) {
+    newDict[dict[key]].push(key);
   } else {
-    new_dict[dict[key]] = [key];
+    newDict[dict[key]] = [key];
   }
 }
-console.log(new_dict);
+console.log(newDict);
