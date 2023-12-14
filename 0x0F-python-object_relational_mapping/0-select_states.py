6ded99f0129b7a2d7322291d4c3147477e3c77be
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-14 15:21:21 +0100

    0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for state in rows:
+        print(state)
+
+    cursor.close()
+    db.close()
