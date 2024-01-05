c3cdb664b4400b2c3b84f864afbbc8b4131a3482
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-05 19:39:07 +0100

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use curl to send a POST request with form data including email and subject to the specified URL
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# send a POST request with form data including email and subject to specified URL
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
