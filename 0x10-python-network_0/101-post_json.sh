a64816acea377926a9ec4760cef091a21227b20a
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-05 20:23:23 +0100

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request with JSON data from the file 'my_json_0' to the specified URL.
-curl -sX POST -H "Content-Type: application/json" -d "@$1" "$2"
+curl -sX POST -H "Content-Type: application/json" -d "@$2" "$1"
