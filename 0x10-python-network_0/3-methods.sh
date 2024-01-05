4836665d8e0684e0aea59f863d1a301e4ecba88c
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-05 19:17:58 +0100

    3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to fetch the HTTP headers, extract the 'Allow' header, and display its values
-curl -sI "$1" | grep Allow | cut -d ' ' -f2,3,4
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
