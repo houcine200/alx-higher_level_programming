d84b654bdf1227039ce9c96139a2a28e4f328b10
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-05 17:58:43 +0100

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Fetches the "Content-Length" from the HEAD response of 0.0.0.0:5000
-curl -sI 0.0.0.0:5000 | grep Content-Length | cut -d ' ' -f2
+curl -sI $1 | grep Content-Length | cut -d ' ' -f2
