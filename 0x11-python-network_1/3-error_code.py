954f975111dffd58dea141a3212a964850b0a19d
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 18:09:53 +0100

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 '''Fetches the content from a specified URL, prints it if successful,
  or prints the HTTP status code if an error occurs'''
 from sys import argv
-from urllib import request
+from urllib import request, error
 
 if __name__ == "__main__":
     url = argv[1]
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         with request.urlopen(url) as response:
             content = response.read().decode('utf-8')
         print(content)
-    except Exception as err:
+    except error.HTTPError as err:
         print(f"Error code: {err.code}")
