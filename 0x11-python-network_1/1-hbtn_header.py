052706b2b279cdd507bc465d21928152805404f2
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 13:13:56 +0100

    1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Fetches the X-Request-Id variable from the header of
+ the HTTP response for a given URL'''
+import urllib.request
+from sys import argv
+
+url = argv[1]
+with urllib.request.urlopen(url) as response:
+    print(response.headers["X-Request-Id"])
