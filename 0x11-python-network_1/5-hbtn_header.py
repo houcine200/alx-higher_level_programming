27d7a5baa926530ac67df8c5cdd1e7ec10e206b3
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 19:45:51 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''Sends a GET request to the specified URL using the 'requests' library,
+retrieve and print value of'X-Request-Id' header from the HTTP response'''
+
+import requests
+from sys import argv
+
+url = argv[1]
+r = requests.get(url)
+print(r.headers['X-Request-Id'])
