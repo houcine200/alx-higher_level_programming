0c7315d1b05340c61e77f92e74ab6a2432be1f71
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 19:03:10 +0100

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''Sends a GET request to url using the 'requests' library,
+prints details about the HTTP response, including type and content.'''
+import requests
+
+html = requests.get('https://alx-intranet.hbtn.io/status')
+
+print("Body response:")
+print(f"\t- type: {type(html.text)}")
+print(f"\t- content: {html.text}")
