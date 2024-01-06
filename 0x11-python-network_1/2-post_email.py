03fc122041c7791a1ba7627e0a67c3bc05d5bf70
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 16:02:31 +0100

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+'''Sends a POST request to a specified URL with an email parameter,
+ and displays the decoded content of the response'''
 
-import urllib.request
 from sys import argv
+from urllib import request, parse
 
 if __name__ == "__main__":
     url = argv[1]
-    with urllib.request.urlopen(url) as response:
-        pass
+    params = {'email': argv[2]}
+    querystring = parse.urlencode(params)
+    data = querystring.encode('utf-8')
+
+    with request.urlopen(url, data) as response:
+        content = response.read().decode('utf-8')
+    print(content)
