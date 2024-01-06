016d781739515ca0d9e288c83518026121aa0190
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 19:46:30 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,7 @@ retrieve and print value of'X-Request-Id' header from the HTTP response'''
 import requests
 from sys import argv
 
-url = argv[1]
-r = requests.get(url)
-print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
