3db2dcc32b8d664fc404e4a56d646e43f2033386
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 20:08:57 +0100

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+''' Sends a POST request to  URL with provided email as a parameter,
+ retrieves the content of the HTTP response, and prints it.'''
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+    r = requests.post(url, data)
+    print(r.text)
