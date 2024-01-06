cc472c99fd070ed5f0ff30120b07b502103ebdf9
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-06 20:52:50 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''Sends a GET request to URL using 'requests' library, print content of
+HTTP response or the error code if the status code is 4xx or higher.'''
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if (r.status_code >= 400):
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
