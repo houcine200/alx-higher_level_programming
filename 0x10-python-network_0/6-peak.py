fe1bf432c91a8cacc4500dfe414d6cd844790301
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-07 18:34:31 +0100

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,18 @@
-#!/usr/bin/python3
-"""
-Finds a peak in a list of unsorted integers
-"""
-
-
 def find_peak(list_of_integers):
+    """ Finds a peak in an unsorted array """
+
     if not list_of_integers:
         return None
-    return max(list_of_integers)
+
+    first = 0
+    second = len(list_of_integers) - 1
+
+    while first < second:
+        mid = (first + second) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            second = mid
+        else:
+            first = mid + 1
+
+    return list_of_integers[first]
