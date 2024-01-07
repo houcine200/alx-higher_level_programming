4de0a24c4c931660ce6850976c591bc8117c29d9
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-07 12:50:06 +0100

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,11 +18,11 @@ if __name__ == '__main__':
 
     try:
         json_data = r.json()
-        id = json_data.get('id')
-        name = json_data.get('name')
-        if not json_data:
+        if 'id' not in json_data or 'name' not in json_data:
             print('No result')
         else:
+            id = json_data.get('id')
+            name = json_data.get('name')
             print(f"[<{id}>] <{name}>")
     except ValueError:
         print("Not a valid JSON")
