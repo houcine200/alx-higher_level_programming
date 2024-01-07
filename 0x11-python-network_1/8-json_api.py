cd9dedc3d2b96a4f5948022d1aa496774d41657b
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-07 13:02:01 +0100

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,11 +4,11 @@ Sends a POST request to URL with a search letter as a parameter.
 If the response contains valid JSON, displays the id and name,
 otherwise handles errors.
 '''
-from sys import argv
+import sys
 import requests
 
 if __name__ == '__main__':
-    av1 = "" if len(argv) == 1 else argv[1]
+    av1 = "" if len(sys.argv) == 1 else sys.argv[1]
 
     data = {'q': av1}
     r = requests.post('http://0.0.0.0:5000/search_user', data)
