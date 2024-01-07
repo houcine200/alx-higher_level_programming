63e35f8a74306b21a4fdf65097b1cecc805754ad
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-07 13:32:37 +0100

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+Uses Basic Authentication with GitHub API to display the user's id.
+Takes GitHub credentials (username and personal access token) as arguments.
+'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    url = 'https://api.github.com/user'
+
+    r = requests.get(url, auth=(username, password))
+    print(r.json().get('id'))
