bca425131b99adc838ca541be5027a4ee0585a78
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-07 12:56:43 +0100

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ from sys import argv
 import requests
 
 if __name__ == '__main__':
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    av1 = "" if len(sys.argv) == 1 else argv[1]
 
     data = {'q': av1}
     r = requests.post('http://0.0.0.0:5000/search_user', data)
